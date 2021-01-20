9b073d9a22796e5554c4b0b613be371bccb632b6
Volodymyr Drobot <vova.drobot@icloud.com>
2021-01-20 04:39:35 +0200

    Initial Commit

---
+++ Menu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Menu
+//
+//  Created by macbook on 20.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
